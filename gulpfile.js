ca431b556f9a108a92f0868f1baf9baba1ae54b9
Tomoko Yokota <yokota@gaji.jp>
2018-08-21 15:20:23 +0900

    add gulp plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,16 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
 const sassGlob = require('gulp-sass-glob');
-const postcss    = require('gulp-postcss');
-const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('autoprefixer');
+const plugins = require('gulp-load-plugins')();
   
-gulp.task('sass', function () {
+gulp.task('scss', () => {
   return gulp
     .src('./stylesheets/sp/main.scss')
-    .pipe(sassGlob())
-    .pipe(sourcemaps.init())
-    .pipe(postcss([autoprefixer()]))
-    .pipe(sourcemaps.write('.'))
-    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(plugins.sassGlob())
+    .pipe(plugins.sass.sync().on('error', plugins.sass.logError))
+    .pipe(plugins.sourcemaps.init())
+    .pipe(plugins.postcss([autoprefixer({ browsers: ['last 4 versions', 'ie >= 11'] })]))
+    .pipe(plugins.sourcemaps.write('.'))
     .pipe(gulp.dest('./stylesheets/css'));
 });
  

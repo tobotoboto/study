c22b9be79f9fa975d48f00929bec467d9a7412a9
Tomoko Yokota <yokota@gaji.jp>
2018-08-21 15:26:55 +0900

    add svg sprite task

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,51 @@ gulp.task('scss', () => {
     .pipe(plugins.sourcemaps.write('.'))
     .pipe(gulp.dest('./stylesheets/css'));
 });
- 
+
+gulp.task('sprite', () => {
+  return gulp
+    .src('./images/icons/svg/*.svg')
+    .pipe(plugins.svgmin())
+    .pipe(plugins.svgstore({ inlineSvg: true }))
+    .pipe(plugins.cheerio({
+      run: ($, file) => {
+        $('style,title,defs').remove();
+        $('[id]:not(symbol)').removeAttr('id');
+        $('[class^="st"],[class^="cls"]').removeAttr('class');
+        $('[style]:not(svg)').removeAttr('style');
+        $('[data-name]').removeAttr('data-name');
+        $('[fill]').removeAttr('fill');
+        $('[stroke]').removeAttr('stroke');
+        $('svg').attr({
+          style: 'display:none'
+        });
+        const symbols = $('svg > symbol').map(function () {
+          return {
+            id: $(this).attr('id')
+          };
+        }).get();
+
+        //確認ページ作成
+        gulp
+          .src('./images/src/_template.html')
+          .pipe(plugins.template({
+            inlineSvg: $('svg'),
+            symbols: symbols
+          }))
+          .pipe(plugins.rename('_svg-icons.html'))
+          .pipe(gulp.dest('./images/src'))
+          .pipe(plugins.preprocess())
+          .pipe(gulp.dest('./images/src'));
+      },
+      //これがないと属性が全部小文字に
+      parserOptions: {
+        xmlMode: true
+      }
+    }))
+    .pipe(plugins.rename('sprite.min.svg'))
+    .pipe(gulp.dest('./images/src'));
+});
+
 gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./stylesheets/**/*.scss', ['sass']);
 });
